54b14c64d92d2c38a3628e8b9360a8634217e260
Arvin Fernandez <arvinf07@gmail.com>
2021-01-07 13:25:05 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,16 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :comments
+
+  def categories_attributes=(hash_of_attr)
+    hash_of_attr.values.each do |category_attr|
+      unless category_attr[:name].blank?
+        category = Category.find_or_create_by(category_attr)
+        self.post_categories.build(category: category)
+      end  
+    end  
+  end  
 
 
 end
